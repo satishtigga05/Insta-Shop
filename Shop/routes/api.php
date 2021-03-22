5bb8337ff43256c9997dadf55d6d660829e01757
SATISH KUMAR TIGGA <53469223+satishtigga05@users.noreply.github.com>
2021-03-22 20:19:00 +0530

    Initial Commit

--- Shop/routes/api.php
+++ Shop/routes/api.php
@@ -1,1 +1,26 @@
+<?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Product Routes
+
+Route::any('add','ProductController@add');
+Route::any('update','ProductController@update');
+Route::any('delete','ProductController@delete');
+Route::any('show','ProductController@show');
